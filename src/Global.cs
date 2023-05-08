1a98ef46502b69270a2122a4206f825313e20513
Emmanuel D. S. Hamani <60195126+OblivCode@users.noreply.github.com>
2023-05-08 16:52:42 +0100

    Add files via upload

---
+++ src/Global.cs
@@ -0,0 +1,243 @@
+﻿using NAudio.Wave;
+using Newtonsoft.Json;
+using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MusicPlayerConsole
+{
+    public static class Global
+    {
+        public readonly static string[] SUPPORTED_FILE_EXTENSIONS = new string[3] { "mp3", "wav", "m4a" };
+        readonly static string RESOURCES_PATH = Directory.GetCurrentDirectory() + "/resources";
+        readonly static string CONFIG_FILENAME = RESOURCES_PATH + "/config.json";
+        
+
+        public static Config LoadConfig()
+        {
+            //Console.WriteLine(CONFIG_FILENAME);
+            string json_text;
+           try
+            {
+                json_text = File.ReadAllText(CONFIG_FILENAME);
+            }
+            catch (FileNotFoundException)
+            {
+                File.Create(CONFIG_FILENAME).Close();
+                return LoadConfig();
+            }
+            catch(DirectoryNotFoundException)
+            {
+                Directory.CreateDirectory(RESOURCES_PATH);
+                File.Create(CONFIG_FILENAME).Close();
+                return LoadConfig();
+            }
+            
+            Config? config = JsonConvert.DeserializeObject<Config>(json_text);
+            if(config == null)
+            {
+                config = new Config();
+                config.controls = new Controls()
+                {
+                    PreviousSound = ConsoleKey.Q,
+                    NextSound = ConsoleKey.E,
+                    PauseSound = ConsoleKey.F,
+                    PlaySound = ConsoleKey.G,
+                    Up = ConsoleKey.UpArrow,
+                    Down = ConsoleKey.DownArrow,
+                };
+            }
+
+            return config;
+        }
+        public static void SaveConfig(Config config)
+        {
+            string config_out = JsonConvert.SerializeObject(config);
+            File.WriteAllText(CONFIG_FILENAME, config_out);
+        }
+        private static string FormatSoundName(string name)
+        {
+            string formatted = "";
+            char[] bad_chars = new char[3] { '[', ']', '.' };
+
+            foreach (char c in name)
+            {
+                if (!bad_chars.Contains(c))
+                    formatted += c;
+            }
+            return formatted;
+        }
+
+
+
+        public struct Controls
+        {
+            public ConsoleKey PreviousSound;
+            public ConsoleKey NextSound;
+            public ConsoleKey PauseSound;
+            public ConsoleKey PlaySound;
+            public ConsoleKey Up;
+            public ConsoleKey Down;
+        }
+        public class Config
+        {
+            public List<string> sound_paths { get; set; }
+            public Controls controls { get; set; }
+            public Config()
+            {
+                sound_paths = new List<string>();
+            }
+            public List<string> GetSoundPaths() => sound_paths;
+            public int GetSoundPathsCount() => sound_paths.Count;
+            public void SetSoundPaths(IEnumerable<string> sound_paths) => this.sound_paths = sound_paths.ToList();
+            public void AddSoundPath(string path) => sound_paths.Add(path);
+        }
+
+        public class Sound
+        {
+            string FILENAME;
+
+            public string GetName(bool with_extension = false)
+            {
+                if(with_extension)
+                    return Path.GetFileName(FILENAME);
+                else
+                    return FormatSoundName(Path.GetFileNameWithoutExtension(FILENAME));
+            }
+
+            public string GetFilename() => FILENAME;
+
+            public static Sound FromFile(string filename)
+            {
+                return new Sound(filename);
+            }
+            public Sound(string filename)
+            {
+                //Console.WriteLine(filename);
+                FILENAME = filename;
+                
+            }
+        }
+
+        public class Playlist
+        {
+            AudioFileReader AUDIO_FILE_READER;
+            WaveOutEvent WAVE_OUT_EVENT;
+            LinkedList<Sound> LIST;
+            public Grid GRID = new Grid();
+            int INDEX_SELECTION = 0, INDEX_PLAYING = -1, MAX_PER_WINDOW = 25, WINDOW_NUM = 0;
+            string FONT_NORMAL, FONT_SELECTED, FONT_PLAYING;
+            bool PLAYING_SOUND = false;
+
+            public Playlist(IEnumerable<Sound> playlist, (string, string, string) fonts)
+            {
+                LIST = new LinkedList<Sound>(playlist);
+                FONT_NORMAL = fonts.Item1; FONT_SELECTED = fonts.Item2; FONT_PLAYING = fonts.Item3;
+
+                
+
+                Refresh();
+            }
+
+            public void Play(bool change = false)
+            {
+                if (PLAYING_SOUND)
+                {
+                    if (INDEX_SELECTION == INDEX_PLAYING)
+                        return;
+                    else if(INDEX_SELECTION != INDEX_PLAYING)
+                    {
+                        INDEX_PLAYING = INDEX_SELECTION;
+                        WAVE_OUT_EVENT.Stop();
+                    }
+                }
+                else if (INDEX_PLAYING == -1 || INDEX_SELECTION != INDEX_PLAYING)
+                    INDEX_PLAYING = INDEX_SELECTION;
+
+                if(change)
+                {
+                    Sound sound = LIST.ElementAt(INDEX_PLAYING);
+                    string filename = sound.GetFilename();
+
+                    AUDIO_FILE_READER = new AudioFileReader(filename);
+                    WAVE_OUT_EVENT = new WaveOutEvent();
+                    WAVE_OUT_EVENT.Init(AUDIO_FILE_READER);
+                }
+
+                WAVE_OUT_EVENT.Play();
+                PLAYING_SOUND = true;
+            }
+
+            public void Pause()
+            {
+                if (!PLAYING_SOUND)
+                    return;
+
+                WAVE_OUT_EVENT.Pause();
+                PLAYING_SOUND = false;
+            }
+
+            public List<Sound> GetList() => LIST.ToList();
+            public int GetSelectionIndex() => INDEX_SELECTION;
+            public int GetPlayingIndex() => INDEX_PLAYING;
+            public int SoundCount
+            {
+                get { return LIST.Count; }
+            }
+            public Grid GetGrid() => GRID;
+
+            void Refresh()
+            {
+                int index_start = 0;
+                if(INDEX_SELECTION > MAX_PER_WINDOW)
+                {
+                    int window_num = (INDEX_SELECTION / MAX_PER_WINDOW) ; //30 / 28 = 1
+                    index_start = MAX_PER_WINDOW * window_num; //
+                }
+                GRID = new Grid();
+                GRID.AddColumn();
+
+                
+
+                for(int i = index_start; i < LIST.Count; i++)
+                {
+                    Sound sound = LIST.ElementAt(i);
+                    string font;
+
+                    if (INDEX_SELECTION == i)
+                        font = FONT_SELECTED;
+                    else if (INDEX_PLAYING == i)
+                        font = FONT_PLAYING;
+                    else
+                        font = FONT_NORMAL;
+
+                    string name = FormatSoundName(sound.GetName());
+                    
+                    GRID.AddRow(new string[] { $"[{font}]{name}[/]" });
+
+                    if (i - index_start > MAX_PER_WINDOW) //break if reached end of window
+                        break;
+                }
+            }
+
+            
+            public void MoveSelectionIndex(int increment = 1)
+            {
+                INDEX_SELECTION += increment;
+
+                if (INDEX_SELECTION < 0)
+                    INDEX_SELECTION = LIST.Count - 1;
+                else if (INDEX_SELECTION > LIST.Count - 1)
+                    INDEX_SELECTION = 0;
+                Console.WriteLine(INDEX_SELECTION);
+                Refresh();
+            }
+        }
+
+
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,215 @@
+﻿using Spectre.Console;
+using System.Data;
+using System.Text.Json;
+using static MusicPlayerConsole.Global;
+using Rule = Spectre.Console.Rule;
+
+namespace MusicPlayerConsole
+{
+    
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            new Program().OnStart();
+        }
+
+
+        public void OnStart()
+        {
+            var (playlist, controls) = LoadConfigSettings();
+            string layout_left_id = "Playlist", layout_right_id = "Right", layout_right_row1_id = "Playing", layout_right_row2_id = "Next",
+                layout_right_row3_id = "Controls";
+            Layout layout = new Layout("Root")
+                .SplitColumns(
+                new Layout(layout_left_id),
+                new Layout(layout_right_id)
+                .SplitRows(
+                    new Layout(layout_right_row1_id),
+                    new Layout(layout_right_row2_id),
+                    new Layout(layout_right_row3_id)
+                    ));
+
+            var controls_layout_string = string.Join("\n", new string[] {
+                "Go to previous: "+controls.PreviousSound,
+                "Go to next: "+controls.NextSound,
+                "Pause: "+controls.PauseSound,
+                "Play: "+controls.PlaySound
+            });
+            layout[layout_right_id]["Controls"].Update(new Panel(
+                controls_layout_string
+                ));
+            
+            layout[layout_left_id].Size(80);
+            AnsiConsole.Live(layout)
+                .Start(ctx =>
+                {
+                    void PreviousSound()
+                    {
+                        int index_prev_sound = playlist.GetPlayingIndex() - 1; // 4
+                        if (index_prev_sound < 0)
+                            index_prev_sound = playlist.SoundCount - 1;
+                        int index_selection = playlist.GetSelectionIndex(); // 7
+                        int difference = index_prev_sound - index_selection; // -3
+
+                        playlist.MoveSelectionIndex(difference);
+                        playlist.Play(true);
+                        UpdatePlaying();
+                    }
+                    void NextSound()
+                    {
+                        int index_next_sound = playlist.GetPlayingIndex() + 1;
+                        if (index_next_sound == playlist.SoundCount)
+                            index_next_sound = 0;
+                        int index_selection = playlist.GetSelectionIndex();
+                        int difference = index_next_sound - index_selection;
+
+                        playlist.MoveSelectionIndex(difference);
+                        playlist.Play(true);
+                        UpdatePlaying();
+                    }
+                    void PauseSound()
+                    {
+                        playlist.Pause();
+                    }
+                    void PlaySound()
+                    {
+                        if (playlist.GetSelectionIndex() != playlist.GetPlayingIndex())
+                        {
+                            playlist.Play(true);
+                            UpdatePlaying();
+                        }
+                        else 
+                            playlist.Play(false);
+                        
+                    }
+                    void UpdatePlaying()
+                    {
+                        int current_sound_index = playlist.GetPlayingIndex();
+                        int next_sound_index = current_sound_index + 1;
+                        if (next_sound_index == playlist.SoundCount)
+                            next_sound_index = 0;
+                        //update current playing panel
+                        layout[layout_right_id][layout_right_row1_id].Update(new Panel(
+                            "Playing: " + playlist.GetList()[current_sound_index].GetName()
+                            ));
+                        //update next panel
+                        layout[layout_right_id][layout_right_row2_id].Update(new Panel(
+                            "Next: "+playlist.GetList()[next_sound_index].GetName()
+                        ));
+                    }
+                    void Refresh()
+                    {
+                        
+                        //update playlist
+                        layout[layout_left_id].Update(
+                        new Panel(
+                            Align.Left(
+                            playlist.GetGrid(),
+                            VerticalAlignment.Middle))
+                        .Expand());
+                        AnsiConsole.Clear();
+                        ctx.Refresh();
+                    }
+
+                    Refresh();
+                    while (true)
+                    {
+                        ConsoleKey key = Console.ReadKey().Key;
+
+                        if (key == controls.Up)
+                            playlist.MoveSelectionIndex(-1);
+                        else if (key == controls.Down)
+                            playlist.MoveSelectionIndex();
+                        else if (key == controls.PlaySound)
+                            PlaySound();
+                        else if (key == controls.PauseSound)
+                            PauseSound();
+                        else if (key == controls.NextSound)
+                            NextSound();
+                        else if (key == controls.PreviousSound)
+                            PreviousSound();
+                        else if (key == ConsoleKey.Escape)
+                            Environment.Exit(0);
+ 
+
+                        Refresh();
+                    }
+                });
+
+            
+        }
+        
+
+        static List<Sound> GetSoundsFromDirectory(string directory)
+        {
+            List<Sound> list = new List<Sound>();
+
+            
+            foreach (string entry_name in Directory.GetFileSystemEntries(directory))
+            {
+                //if entry is a file
+                if (File.Exists(entry_name))
+                {
+                    //if file ext is in supported list
+                    string entry_ext = Path.GetExtension(entry_name)[1..];
+
+                    if (SUPPORTED_FILE_EXTENSIONS.Contains(entry_ext))
+                    {
+                        list.Add(Sound.FromFile(entry_name));
+                    }
+                }
+                else if (Directory.Exists(entry_name)) //if entry is a directory
+                    list.AddRange(GetSoundsFromDirectory(entry_name));
+            }
+
+            return list;
+        }
+
+        (Playlist, Controls) LoadConfigSettings()
+        {
+            Config config = LoadConfig();
+
+            if (config.GetSoundPathsCount() == 0)
+            {
+                //get a path to load sound files from
+                while(true)
+                {
+                    string sound_path = AnsiConsole.Ask<string>("Input a path to load song files:");
+
+                    if (!Directory.Exists(sound_path))
+                        AnsiConsole.MarkupLine("[red]Invalid directory.[/] Try again!");
+                    else
+                    {
+                        config.AddSoundPath(sound_path);
+                        
+                        SaveConfig(config);
+                        break;
+                    }
+                }
+            }
+            //get sound filenames
+            List<Sound> sounds = new List<Sound>();
+            int sound_path_count = config.GetSoundPaths().Count;
+            AnsiConsole.Progress().Start(ctx =>
+            {
+                var task_load_sounds = ctx.AddTask($"Loading sounds from {sound_path_count} directories");
+                float task_load_sounds_step = 100 / sound_path_count;
+
+                foreach (string sound_path in config.GetSoundPaths())
+                {
+                    sounds.AddRange(GetSoundsFromDirectory(sound_path));
+                    task_load_sounds.Increment(task_load_sounds_step);
+                }
+                
+            });
+
+            Playlist playlist = new Playlist(sounds, ("white on black", "black on white", "green on black"));
+            Controls controls = config.controls;
+            return (playlist, controls);
+        }
+
+        
+    }
+}
